2cfae763888fc0ca26e5417297e39c036fdc311c
Jacob Laux <jake@esolutionsone.com>
2022-12-06 21:54:57 -0600

    adding day 6

---
+++ Day6/AOC_Day6_pt2.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+input  = ''.join([line.rstrip() for line in open('Day6/day-6-input.txt')])
+setup  = input
+buffer = input[0:14]
+input  = input[14:]
+garbo  = ''
+#for each character 
+for char in input:
+    num_uniq = len(Counter(buffer))
+    if num_uniq == len(buffer):
+        print(buffer)
+        print(len(garbo)+len(buffer))
+        break
+    else:
+        first_char = buffer[0]
+        buffer = buffer[1:]
+        buffer += char
+        garbo += first_char
