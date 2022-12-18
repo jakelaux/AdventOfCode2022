a2b74edf9848f3e740e608aec13ebcc54cdcfcb7
Jacob Laux <lauxx180@gmail.com>
2022-12-18 16:56:18 -0600

    pushing up changes in progress day 8 pt1 & 2

--- Day8/AOC_Day8.py
+++ Day8/AOC_Day8.py
@@ -3,73 +3,44 @@ file = 'Day8/day-8-test-input.txt'#'Day8/day-8-input.txt'
 trees = open(file).read().splitlines()
 for i, row in enumerate(trees):
     forest.append(list(row))
-visible = 0
-visible = (((len(forest[0])*2) + (len(forest)*2)) - 4)
+rows = len(forest)
+cols = len(forest[0])
+visible = (rows*2) + (cols*2) - 4
+highest = 1
 
-#def checkVisibile(row, col, tree):
-#    print(row,col,tree)
-#    global visible
-#    l = len(forest[row][:col])-1
-#    r = len(forest[row][col:])-1
-#    u = len(forest[:row])-1
-#    d = len(forest[row:])-1
-#    lv = []
-#    rv = []
-#    uv = []
-#    dv = []
-#    tree_visible = 0
-#    while l > 0:
-#        l -= 1
-#        if forest[row][l] < tree:
-#            lv.append({'comp':forest[row][l],'tree':tree})
-#        if len(lv) == l+1:
-#            tree_visible += 1
-#    while r > 0:
-#        r -= 1
-#        if forest[row][r] < tree:
-#            rv.append(({'comp':forest[row][r],'tree':tree}))
-#        if len(rv) == r+1:
-#            tree_visible += 1
-#    while u > 0:
-#        u -= 1
-#        if forest[u][col] < tree:
-#            uv.append(({'comp':forest[u][col],'tree':tree}))    
-#        if uv == u+1:
-#            tree_visible += 1
-#    while d > 0:
-#        d -= 1
-#        if forest[d][col] < tree:
-#            dv.append(({'comp':forest[d][col],'tree':tree}))
-#        if dv == d+1:
-#            tree_visible += 1
-#    if tree_visible > 0:
-#        visible += 1
+def isVisible(l,r,u,d,tree):
+    ml = (max(l)<tree) 
+    mr = (max(r)<tree) 
+    mu = (max(u)<tree) 
+    md = (max(d)<tree)
+    return ml or mr or mu or md
 
-def checkVisibile(r,c,tree):
-    global forest
-    l  = len(forest[r][:c])-1
-    rt = len(forest[r][c:])-1
-    u  = [forest[row][c] for row in range(r)]
-    d  = [forest[row][c] for row in range(r+1,len(forest[r]))]
-    up    = map(lambda comp: comp < tree, u)
-    down  = map(lambda comp: comp < tree, d)
-    left  = map(lambda comp: comp < tree, forest[r][:l])
-    right = map(lambda comp: comp < tree, forest[r][rt+1:])
-    print(up,down,left,right,r,c,tree)
-    return up or down or left or right
+def countTrees(comp,tree):
+    dir_total = 1
+    for c in comp:
+        if c < tree:
+            dir_total += 1
+        elif c <= tree:
+            dir_total += 1
+            return dir_total
+    return dir_total
 
-rows = []
-for r, row in enumerate(forest):
-    skip = False
-    rows.append(row)
-    if r == 0 or (r+1) == len(forest):
-        continue
-    for c, tree in enumerate(row):
-        if c == 0 or (c+1) == len(row):
-            continue
-        if(not checkVisibile(r,c,tree)):
+#updated to use range to skip edges that were included in my original enumerate loop
+for row in range(1, rows-1):
+    for col in range(1, cols-1):
+        tree = forest[row][col]
+        #pt1
+        #for left, right, up, and down > if the tallest tree in the list is shorter than our tree our tree is visible
+        l = [ forest[row][col-i] for i in range(1, col+1)    ]
+        r = [ forest[row][col+i] for i in range(1, cols-col) ]
+        u = [ forest[row-i][col] for i in range(1, row+1)    ]
+        d = [ forest[row+i][col] for i in range(1, rows-row) ]
+        if isVisible(l,r,u,d,tree):
             visible += 1
+        score = countTrees(l,tree)*countTrees(r,tree)*countTrees(u,tree)*countTrees(d,tree)
+        print(score)
+        if score > highest:
+            highest = score
 
-print(visible)
-for r in forest:
-    print(r)
+print("pt1:", visible)
+print("pt2: ", highest)
