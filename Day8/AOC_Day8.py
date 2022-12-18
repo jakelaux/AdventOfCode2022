05439d62304ce092432150e7a79282cb76e6d721
Jacob Laux <jake@esolutionsone.com>
2022-12-18 15:33:35 -0600

    continued debugging

--- Day8/AOC_Day8.py
+++ Day8/AOC_Day8.py
@@ -1,45 +1,75 @@
-forest  = []
+forest = []
 file = 'Day8/day-8-test-input.txt'#'Day8/day-8-input.txt'
-trees   = [line.rstrip() for line in open(file)]
-count   = 0;
+trees = open(file).read().splitlines()
 for i, row in enumerate(trees):
     forest.append(list(row))
+visible = 0
 visible = (((len(forest[0])*2) + (len(forest)*2)) - 4)
 
-def checkTree(index,tree,index_setter,dir):
-    is_visible = True
-    i=index_setter
-    while i >= 0:
-        i-=1
-        comp_tree = 0
-        if dir=='updown':
-            comp_tree = forest[index][i]
-        elif dir=='leftright':
-            comp_tree = forest[i][index]
-        if tree <= comp_tree:
-            continue
-        else:
-            is_visible = False
-            break
-    return is_visible
+#def checkVisibile(row, col, tree):
+#    print(row,col,tree)
+#    global visible
+#    l = len(forest[row][:col])-1
+#    r = len(forest[row][col:])-1
+#    u = len(forest[:row])-1
+#    d = len(forest[row:])-1
+#    lv = []
+#    rv = []
+#    uv = []
+#    dv = []
+#    tree_visible = 0
+#    while l > 0:
+#        l -= 1
+#        if forest[row][l] < tree:
+#            lv.append({'comp':forest[row][l],'tree':tree})
+#        if len(lv) == l+1:
+#            tree_visible += 1
+#    while r > 0:
+#        r -= 1
+#        if forest[row][r] < tree:
+#            rv.append(({'comp':forest[row][r],'tree':tree}))
+#        if len(rv) == r+1:
+#            tree_visible += 1
+#    while u > 0:
+#        u -= 1
+#        if forest[u][col] < tree:
+#            uv.append(({'comp':forest[u][col],'tree':tree}))    
+#        if uv == u+1:
+#            tree_visible += 1
+#    while d > 0:
+#        d -= 1
+#        if forest[d][col] < tree:
+#            dv.append(({'comp':forest[d][col],'tree':tree}))
+#        if dv == d+1:
+#            tree_visible += 1
+#    if tree_visible > 0:
+#        visible += 1
 
-def checkVisibile(row,column,tree):
-    if checkTree(column,tree,column,'updown') or checkTree(column,tree,len(forest)-column,'updown') or checkTree(row,tree,row,'leftright') or checkTree(row,tree,len(forest[0])-row,'leftright'):
-        return True
-    return False
+def checkVisibile(r,c,tree):
+    global forest
+    l  = len(forest[r][:c])-1
+    rt = len(forest[r][c:])-1
+    u  = [forest[row][c] for row in range(r)]
+    d  = [forest[row][c] for row in range(r+1,len(forest[r]))]
+    up    = map(lambda comp: comp < tree, u)
+    down  = map(lambda comp: comp < tree, d)
+    left  = map(lambda comp: comp < tree, forest[r][:l])
+    right = map(lambda comp: comp < tree, forest[r][rt+1:])
+    print(up,down,left,right,r,c,tree)
+    return up or down or left or right
 
 rows = []
-for i, row in enumerate(forest):
+for r, row in enumerate(forest):
     skip = False
     rows.append(row)
-    if i == 0 or (i+1) == len(forest):
+    if r == 0 or (r+1) == len(forest):
         continue
-    for j, tree in enumerate(row):
-        if j == 0 or (j+1) == len(row):
+    for c, tree in enumerate(row):
+        if c == 0 or (c+1) == len(row):
             continue
-        elif checkVisibile(i,j,tree):
+        if(not checkVisibile(r,c,tree)):
             visible += 1
 
 print(visible)
-for row in rows:
-    print(row)
+for r in forest:
+    print(r)
